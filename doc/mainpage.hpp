a1a822103fcbf2860806c4e917bcf3de69f3b49d
caillaud-florian <caillaud.florian@gmail.com>
2014-03-31 20:19:23 +0200

    Mise à jour de la page d'accueil de la documentation (objectif et image) : à améliorer tant pour l'un que pour l'autre.

--- doc/mainpage.hpp
+++ doc/mainpage.hpp
@@ -1,71 +1,16 @@
 ////////////////////////////////////////////////////////////
 /// \mainpage
 ///
-/// \section welcome Welcome
-/// Welcome to the official SFML documentation. Here you will find a detailed
-/// view of all the SFML <a href="./annotated.php">classes</a> and functions. <br/>
-/// If you are looking for tutorials, you can visit the official website
-/// at <a href="http://www.sfml-dev.org/">www.sfml-dev.org</a>.
+/// Bienvenue sur la documentation de la plateforme SARAH (System of Accelerated 3D Rendering Applicatively Handy).
+/// Ici pour pourrez trouver les informations nécessaire à une bonne utilisation de SARAH.
+/// <br/>
+/// <br/>
+/// Le SARAH Project a été lancé afin de proposer un cadre de développement à tous ceux qui aimeraient pouvoir 
+/// concevoir rapidement et simplement un jeu 2D ou un utilitaire 3D par exemple. SARAH est une encapsulation
+/// d'une partie de la <a href="http://www.sfml-dev.org/">SFML</a> permettant de créer un logiciel en quelques minutes.
+/// <br/>
+/// <h3>Alors, à vous de jouer !!</h3>
+/// <br/>
 ///
-/// \section example Short example
-/// Here is a short example, to show you how simple it is to use SFML :
-///
-/// \code
-///
-/// #include <SFML/Audio.hpp>
-/// #include <SFML/Graphics.hpp>
-/// 
-/// int main()
-/// {
-///     // Create the main window
-///     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-/// 
-///     // Load a sprite to display
-///     sf::Texture texture;
-///     if (!texture.loadFromFile("cute_image.jpg"))
-///         return EXIT_FAILURE;
-///     sf::Sprite sprite(texture);
-/// 
-///     // Create a graphical text to display
-///     sf::Font font;
-///     if (!font.loadFromFile("arial.ttf"))
-///         return EXIT_FAILURE;
-///     sf::Text text("Hello SFML", font, 50);
-/// 
-///     // Load a music to play
-///     sf::Music music;
-///     if (!music.openFromFile("nice_music.ogg"))
-///         return EXIT_FAILURE;
-///
-///     // Play the music
-///     music.play();
-/// 
-///     // Start the game loop
-///     while (window.isOpen())
-///     {
-///         // Process events
-///         sf::Event event;
-///         while (window.pollEvent(event))
-///         {
-///             // Close window : exit
-///             if (event.type == sf::Event::Closed)
-///                 window.close();
-///         }
-/// 
-///         // Clear screen
-///         window.clear();
-/// 
-///         // Draw the sprite
-///         window.draw(sprite);
-/// 
-///         // Draw the string
-///         window.draw(text);
-/// 
-///         // Update the window
-///         window.display();
-///     }
-/// 
-///     return EXIT_SUCCESS;
-/// }
-/// \endcode
+/// \image html banner.jpg
 ////////////////////////////////////////////////////////////
